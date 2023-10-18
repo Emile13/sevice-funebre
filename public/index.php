e4b055d6826a4f8bcc779cbc313d5ecf6ceff1ea
TSARATSIRY Jean Emile <45931724+Emile13@users.noreply.github.com>
2023-10-18 20:26:31 +0200

    construction
    V1

---
+++ public/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nom de votre entreprise</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">À propos</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="hero">
+        <h1>Des services funéraires dignes et respectueux</h1>
+        <p>Nous sommes là pour vous aider à traverser cette période difficile.</p>
+        <a href="#" class="cta-button">En savoir plus</a>
+    </section>
+
+    <section class="services">
+        <h2>Nos Services</h2>
+        <ul>
+            <li>Organisation complète des funérailles</li>
+            <li>Crémation et inhumation</li>
+            <li>Assistance administrative</li>
+            <!-- Ajoutez d'autres services ici -->
+        </ul>
+    </section>
+
+    <section class="about">
+        <h2>À propos de nous</h2>
+        <p>Nous sommes une équipe dévouée, prête à vous soutenir dans ce moment difficile.</p>
+    </section>
+
+    <section class="contact">
+        <h2>Contactez-nous</h2>
+        <p>Pour toute question ou demande de renseignements, n'hésitez pas à nous contacter :</p>
+        <a href="tel:0123456789">Téléphone : 01 23 45 67 89</a>
+        <a href="mailto:info@votreentreprise.com">Email : info@votreentreprise.com</a>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Votre Entreprise | Tous droits réservés</p>
+    </footer>
+</body>
+</html>
